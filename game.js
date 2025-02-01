9a85ccbb56f28a196d0be1489b18c2c6959421fb
eliss <elis.staaf@proton.me>
2025-02-01 15:42:25 +0100

    add stop() func

--- game.js
+++ game.js
@@ -32,6 +32,9 @@ var gameCanvas = {
     this.canvas.height = canvasHeight;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  },
+  stop: function() {
+    throw "-- Game ended --"
   }
 }
 
